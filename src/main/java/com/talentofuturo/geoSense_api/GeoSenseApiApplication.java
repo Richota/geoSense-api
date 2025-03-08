be87b3abbb728337ae695f09693d8ad79f86d0e0
Ricardo Suarez <rsuarez.adv@gmail.com>
2025-03-07 23:19:14 -0300

    KickOff

---
+++ src/main/java/com/talentofuturo/geoSense_api/GeoSenseApiApplication.java
@@ -0,0 +1,13 @@
+package com.talentofuturo.geoSense_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoSenseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoSenseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/talentofuturo/geoSense_api/GeoSenseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.talentofuturo.geoSense_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeoSenseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
